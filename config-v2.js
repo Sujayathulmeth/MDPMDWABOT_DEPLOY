8dbd08bae9d1b5a056fefa3fb6a457b7e23a563b
Sujayathulmeth <134757428+Sujayathulmeth@users.noreply.github.com>
2024-12-20 21:02:06 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94718628995"
+const SESSION_ID = "PRABATH-MD~dcYwzZSI#9D3RmBa39ROGcKBCCaMOJ4xPCA9GMu92xIdQwC51Kaw"
 
 
 module.exports = {
